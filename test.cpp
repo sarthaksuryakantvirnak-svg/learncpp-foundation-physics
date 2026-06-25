d99cf4fb3fdb252e46ae9aa9782d532e8ef8385c
sarthaksuryakantvirnak-svg <sarthaksuryakantvirnak@gmail.com>
2026-06-25 13:42:23 +0530

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+namespace physicsConstant
+{
+	double g = 9.81;
+	double k = 9 * e9;
+
+}
+
+int main()
+{
+	std::cout << "Electrostatics Constant (k) = " << physicsConstant::double k;
+}
