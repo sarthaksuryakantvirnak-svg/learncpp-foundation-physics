faeb06ab592019023676dd041ad40a6dc031e723
sarthaksuryakantvirnak-svg <sarthaksuryakantvirnak@gmail.com>
2026-06-29 11:45:11 +0530

    k

---
+++ CMakeProject3/CMakeProject3/CMakeProject3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+void calc(double dt, double u)
+{
+
+	double g = 9.8;
+	double c = 12.5;
+	double m = 68.1;
+	double t{};
+	double k = c / m;
+
+	for (int i = 0; i < 7; i++)
+	{
+		std::cout << "at t = " << t << " velocity = " << u << " m / s \n";
+
+		u = u + (g - k * u) * (dt);
+		
+		
+		t = t + dt;
+	}
+}
+
+int main()
+{
+	calc(2, 0);
+	return 0;
+}
